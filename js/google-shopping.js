e3b744ca8132e71f910768f43ea763adf1733537
brandiw <brandimichelle.0911@gmail.com>
2019-11-21 15:13:49 -0800

    Adding print statements in each problem

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -14,16 +14,31 @@ console.log(data.items[0])
 // `shopping#product`. Print the count of these results. Where else is
 // this count information stored in the search results?
 
+console.log('\nProblem 1:\n')
+
 // 2.) Print the `title` all items with a `backorder` availability
 // in `inventories`.
 
+console.log('\nProblem 2:\n')
+
 // 3.) Print the `title` all items with more than one image link.
+
+console.log('\nProblem 3:\n')
 
 // 4.) Print all "Canon" products in the items
 // HINT: careful with case sensitivity!
 
+console.log('\nProblem 4:\n')
+
 // 5.) Print all `items` that have an author name of "eBay" and are
 // brand "Canon".
+// HINT: What is the type of author?
+
+console.log('\nProblem 5:\n')
 
 // 6.) Print all the products with their **brand**, **price**,
 // and an **image link**
+// HINT: You can just use the first (0th) element in the images
+// and inventories arrays.
+
+console.log('\nProblem 6:\n')
