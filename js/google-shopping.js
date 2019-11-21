c5d555ea332685b5b53a2eba09b11552d4aaafcd
brandiw <brandimichelle.0911@gmail.com>
2019-11-21 14:44:24 -0800

    Starter code for 2019

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,26 @@
-var data = require('../products.json');
+// Require is a node-specific thing, so run
+// your file in the terminal with the node command!
+// e.g., node js/google-shopping.js
+let data = require('../products.json')
 
-// Write your solutions below
+// This is a print out of all the items in the data
+console.log(data.items)
 
-console.log(data.items);
+// YOUR CODE BELOW
+// 1.) Go through the `items` and find all results that have `kind` of
+// `shopping#product`. Print the count of these results. Where else is
+// this count information stored in the search results?
+
+// 2.) Print the `title` all items with a `backorder` availability
+// in `inventories`.
+
+// 3.) Print the `title` all items with more than one image link.
+
+// 4.) Print all "Canon" products in the items
+// HINT: careful with case sensitivity!
+
+// 5.) Print all `items` that have an author name of "eBay" and are
+// brand "Canon".
+
+// 6.) Print all the products with their **brand**, **price**,
+// and an **image link**
